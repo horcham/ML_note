0c76a9e2ae16129f4d3de6047e4e64b43ba07f1b
horcham <690936541@qq.com>
2017-11-11 19:42:56 +0800

    2017.11.11_2 FDA

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-xelatex -shell-escape PatternRecognition.tex
-bibtex PatternRecognition.aux
-xelatex -shell-escape PatternRecognition.tex
-xelatex -shell-escape PatternRecognition.tex
-rm *.aux *.log
-evince PatternRecognition.pdf
+xelatex -shell-escape CS229.tex
+bibtex CS229.aux
+xelatex -shell-escape CS229.tex
+xelatex -shell-escape CS229.tex
+rm *.aux *.log *.bbl *.blg *.toc
+evince CS229.pdf
